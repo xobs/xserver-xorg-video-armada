c063b560a829438ba473e5867dfb90017e80d3c2
Russell King <rmk@arm.linux.org.uk>
2014-09-19 23:51:18 +0100

    common: better mint()/maxt() macros
    Provide better mint() and maxt() macros.
    
    Signed-off-by: Russell King <rmk@arm.linux.org.uk>

--- common/utils.h
+++ common/utils.h
@@ -11,7 +11,21 @@
 	const typeof( ((type *)0)->member ) *__mptr = (ptr); \
 	(type *)( (char *)__mptr - offsetof(type,member) );})
 
-#define mint(x,y)	({(void)(&x == &y); x < y ? x : y; })
-#define maxt(x,y)	({(void)(&x == &y); x < y ? y : x; })
+#define __min(tx,ty,x,y) ({		\
+	tx _min1 = (x);			\
+	ty _min2 = (y);			\
+	(void)(&_min1 == &_min2);	\
+	_min1 < _min2 ? _min1 : _min2; })
+
+#define __max(tx,ty,x,y) ({		\
+	tx _max1 = (x);			\
+	ty _max2 = (y);			\
+	(void)(&_max1 == &_max2);	\
+	_max1 > _max2 ? _max1 : _max2; })
+
+#define mint(x,y)	__min(typeof(x), typeof(y), x, y)
+#define min_t(t,x,y)	__min(t, t, x, y)
+#define maxt(x,y)	__max(typeof(x), typeof(y), x, y)
+#define max_t(t,x,y)	__max(t, t, x, y)
 
 #endif
