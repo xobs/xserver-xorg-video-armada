1f7e93adc48874d4b99576ae4b6cd0496b83a612
Russell King <rmk@arm.linux.org.uk>
2014-11-04 22:02:05 +0000

    etnaviv: dri2: pass name to select appropriate etna driver
    Signed-off-by: Russell King <rmk@arm.linux.org.uk>

--- etnaviv/etnaviv_dri2.h
+++ etnaviv/etnaviv_dri2.h
@@ -7,7 +7,7 @@
 #ifndef VIVANTE_DRI2_H
 #define VIVANTE_DRI2_H
 
-Bool etnaviv_dri2_ScreenInit(ScreenPtr pScreen, int drm_fd);
+Bool etnaviv_dri2_ScreenInit(ScreenPtr pScreen, int drm_fd, const char *name);
 void etnaviv_dri2_CloseScreen(CLOSE_SCREEN_ARGS_DECL);
 void etnaviv_dri2_vblank(int fd, unsigned frame, unsigned tv_sec,
 	unsigned tv_usec, void *event);
