5ac5b734c3fd22b1b493a90e2044e44794014d0a
Russell King <rmk@arm.linux.org.uk>
2015-06-12 18:51:27 +0100

    etnadrm: add dmabuf export and global name import
    Add support for exporting dmabufs from etnadrm, and importing global
    names into etnadrm.  These are required to support Xvbo overlay
    display, and also Xvbo in the etnadrm backend.
    
    It is necessary to also add dummy functions for libetnaviv to avoid
    link or runtime unresolved symbol errors, but we need to be careful
    of clashing with libetnaviv's etna_bo_from_name(), which uses different
    "names".
    
    Signed-off-by: Russell King <rmk@arm.linux.org.uk>

--- etnaviv/etnaviv_compat.h
+++ etnaviv/etnaviv_compat.h
@@ -19,4 +19,13 @@
 #define etnaviv_Key                  int
 #endif
 
+/*
+ * Etnaviv itself does not provide these functions.  We'd like these
+ * to be named this way, but some incompatible etnaviv functions clash.
+ */
+#define etna_bo_from_name my_etna_bo_from_name
+struct etna_bo *etna_bo_from_name(struct viv_conn *conn, uint32_t name);
+#define etna_bo_to_dmabuf my_etna_bo_to_dmabuf
+int etna_bo_to_dmabuf(struct viv_conn *conn, struct etna_bo *bo);
+
 #endif
